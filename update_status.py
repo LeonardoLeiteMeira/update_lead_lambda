682fd283a08ac2635309e1de13de498ce12d1128
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-03-31 23:16:48 -0300

    Fix lambda response

--- update_status.py
+++ update_status.py
@@ -13,6 +13,14 @@ LEADS_DATABASE_ID = os.getenv('LEADS_DATABASE_ID')
 ALGORITHM = os.getenv('JWT_ALGORITHM')
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
+def send_response(status:int, body:str):
+    return {
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        'statusCode': status,
+        'body': body
+    }
 
 def create_jwt(payload: dict):
     return jwt.encode(
@@ -41,11 +49,7 @@ def update_lead_status(event, context):
 
     token_result = decode_jwt(token)
     if not token_result['is_valid']:
-        return {
-            'status': 401,
-            'body': json.dumps(token_result['data']),
-            'message': 'Não autorizado' 
-        }
+        return send_response(401, f"Nao autorizado {token_result['data']}")
     
     payload = token_result['data']
     lead_id = int(payload['lead_id'])
@@ -65,71 +69,13 @@ def update_lead_status(event, context):
 
         current_status = ej['properties']['Lead Status']['status']['name']
         if current_status == 'Refused' or current_status == 'Accept':
-            return {
-                'status': 400,
-                'body': json.dumps('Feedback já enviado!'),
-                'message': 'Feedback já enviado!'
-            }
+            return send_response(400, 'Feedback ja enviado!')
 
         page_id = ej['id']
 
         notion.pages.update(page_id=page_id, properties={'Lead Status': {'status': {'name': status}}})
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Lead status updated successfully'),
-            'message': "Atualizado com sucesso"
-        }
+        return send_response(200, "Atualizado com sucesso")
     except Exception as e:
         print(e)
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Error updating lead status'),
-            'message': 'Error updating lead status'
-        }
-
-
-
-
-# if __name__ == "__main__":
-    # token = create_jwt({"lead_id": 696})
-    # print("\n\n")
-    # print(token)
-    # print("\n\n")
-
-    # print(decode_jwt(token))
-
-    # request ={
-    #     "resource": "/",
-    #     "path": "/",
-    #     "httpMethod": "GET",
-    #     "headers": {
-    #         "Accept": "application/json",
-    #         "Host": "example.execute-api.us-east-1.amazonaws.com",
-    #         "User-Agent": "PostmanRuntime/7.28.4"
-    #     },
-    #     "queryStringParameters": {
-    #         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZWFkX2lkIjo2OTYsImV4cCI6MTc0ODY1MzI4Nn0.gqzWWFfVPZFVdpdoA1dysjgV8RjhJinroSUW6quvan8",
-    #         "status": "Accept"
-    #     },
-    #     "pathParameters": None,
-    #     "stageVariables": None,
-    #     "requestContext": {
-    #         "resourceId": "abcdef",
-    #         "resourcePath": "/minharecurso",
-    #         "httpMethod": "GET",
-    #         "requestId": "1234-5678-91011",
-    #         "accountId": "123456789012",
-    #         "stage": "prod",
-    #         "identity": {
-    #         "sourceIp": "127.0.0.1",
-    #         "userAgent": "PostmanRuntime/7.28.4"
-    #         },
-    #         "apiId": "api-id"
-    #     },
-    #     "body": None,
-    #     "isBase64Encoded": False
-    # }
-    # resp = update_lead_status(request, {"local":True})
-    # print(resp)
-
+        return send_response(500, 'Error updating lead status')
